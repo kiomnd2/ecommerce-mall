c498defb51761aaf4d142e37f9c5a2ea1703db56
김형익 <kiomnd2@naver.com>
2023-04-13 23:44:36 +0900

    test commit

--- src/main/java/kr/ziz/ecommercemall/interfaces/member/MemberApi.java
+++ src/main/java/kr/ziz/ecommercemall/interfaces/member/MemberApi.java
@@ -1,17 +1,14 @@
 package kr.ziz.ecommercemall.interfaces.member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/member")
 public class MemberApi {
 
-    @GetMapping("/join")
+    @PostMapping("/join")
     public String join() {
         return "지장이재석";
     }
