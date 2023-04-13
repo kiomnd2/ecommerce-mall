19593cc6c3aa59ba47062dd63daad17c48168a86
김형익 <kiomnd2@naver.com>
2023-04-13 21:57:09 +0900

    fist commit

---
+++ src/main/java/kr/ziz/ecommercemall/EcommerceMallApplication.java
@@ -0,0 +1,13 @@
+package kr.ziz.ecommercemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMallApplication.class, args);
+    }
+
+}
