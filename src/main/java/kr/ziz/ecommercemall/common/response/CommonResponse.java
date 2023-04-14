09b78a8409619ff003c6844ad98310bae009447d
kiomnd2 <kiomnd2@naver.com>
2023-04-14 21:02:55 +0900

    common class 기초 정의

---
+++ src/main/java/kr/ziz/ecommercemall/common/response/CommonResponse.java
@@ -0,0 +1,37 @@
+package kr.ziz.ecommercemall.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse <T> {
+    private Result result;
+    private T data;
+    private String message;
+    private String errorCode;
+
+    public static <T> CommonResponse<T> success(T data, String message) {
+        return new CommonResponse<>(Result.SUCCESS, data, message, null);
+    }
+
+    public static <T> CommonResponse<T> success(T data) {
+        return success(data, null);
+    }
+
+    public static <T> CommonResponse<T> fail(ErrorCode errorCode, T data) {
+        return new CommonResponse<>(Result.FAIL, data, errorCode.getMessage(), errorCode.name());
+    }
+
+    public static <T> CommonResponse<T> fail(ErrorCode errorCode) {
+        return fail(errorCode, null);
+    }
+
+    public enum Result {
+        SUCCESS, FAIL
+    }
+}
