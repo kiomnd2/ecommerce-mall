35f4dbfb511c97e58587c3ae33af9db4f078e8b4
nabongbonge <jaeseugi.1927@gmail.com>
2023-04-24 23:49:59 +0900

    이메일 유효성 로직 추가 및 컬럼 변경

--- src/test/java/kr/ziz/ecommercemall/domain/manager/fixture/ManagerFixture.java
+++ src/test/java/kr/ziz/ecommercemall/domain/manager/fixture/ManagerFixture.java
@@ -1,11 +1,10 @@
 package kr.ziz.ecommercemall.domain.manager.fixture;
 
-import kr.ziz.ecommercemall.domain.common.Address;
 import kr.ziz.ecommercemall.domain.manager.Manager;
 
 public class ManagerFixture {
   public static Manager createManager(String managerPw) {
     return new Manager("managerId", "managerNm", managerPw
-            , "email", "N", "bizNo", new Address(), "");
+            , "email", "01012345678");
   }
 }
