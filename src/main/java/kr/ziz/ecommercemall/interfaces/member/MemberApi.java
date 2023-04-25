84c66a8d5455e263850f580156e99a5b61ba6595
kiomnd2 <kiomnd2@gamil.com>
2023-04-25 23:50:31 +0900

    member interface 정의

--- src/main/java/kr/ziz/ecommercemall/interfaces/member/MemberApi.java
+++ src/main/java/kr/ziz/ecommercemall/interfaces/member/MemberApi.java
@@ -1,5 +1,6 @@
 package kr.ziz.ecommercemall.interfaces.member;
 
+import kr.ziz.ecommercemall.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,12 +10,33 @@ import org.springframework.web.bind.annotation.*;
 public class MemberApi {
 
     @PostMapping("/join")
-    public String join() {
-        return "회원";
+    public CommonResponse join(@RequestBody MemberDto.RequestJoin requestJoin) {
+        return CommonResponse.success("memberToken");
     }
 
     @PostMapping("/logged-in")
-    public String login() {
-        return "로그인";
+    public CommonResponse login(@RequestBody MemberDto.RequestLogin requestLogin) {
+        return CommonResponse.success("memberToken");
     }
+
+    @PutMapping
+    public CommonResponse update(@RequestBody MemberDto.RequestUpdate requestUpdate) {
+        return CommonResponse.success("수정완료");
+    }
+
+    @DeleteMapping
+    public CommonResponse delete(@RequestBody MemberDto.RequestDelete requestDelete) {
+        return CommonResponse.success("");
+    }
+
+    @GetMapping("/otp/{email}")
+    public CommonResponse issueOtp(@PathVariable("email") String email) {
+        return CommonResponse.success("인증키 발급 되었습니다.");
+    }
+
+    @PostMapping("/otp")
+    public CommonResponse checkOtp() {
+        return CommonResponse.success("토큰 인증 되었습니다.");
+    }
+
 }
